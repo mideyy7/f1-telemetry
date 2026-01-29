af8ee92d33720887c948af2a6406c25091144ab5
Ayomide <mideyy7@gmail.com>
2026-01-29 21:05:40 +0000

    Ring buffer implemented

---
+++ src/ingestion/ring_buffer.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+#include <cstddef>
+
+
+template<typename T>
+class RingBuffer {
+    private:
+        std::vector<T> buffer_;
+        size_t capacity_;
+        std::mutex mutex_;
+        std::condition_variable cv_not_full_;
+        std::condition_variable cv_not_empty_;
+
+        bool shutdown_;
+
+        size_t head_;
+        size_t tail_;
+    
+    public:
+        explicit RingBuffer(size_t capacity);
+
+        bool push(const T &item);
+        bool pop(T &item);
+
+        void shutdown();
+};
+
+// constructor implementation
+template<typename T>
+RingBuffer<T>::RingBuffer(size_t capacity)
+    : buffer_(capacity), capacity_(capacity), shutdown_(false), head_(0), tail_(0) {};
+
+// push implementation
+template<typename T>
+bool RingBuffer<T>::push(const T &item) {
+    std::unique_lock<std::mutex> lock(mutex_);
+
+    if(shutdown_) {
+        return false;
+    }
+
+    size_t next_head = (head_ + 1) % capacity_;
+    if(next_head == tail_) {
+        return false;
+    }
+
+    buffer_[head_] = item;
+    head_ = next_head;
+
+    lock.unlock();
+    cv_not_empty.notify_one();
+
+    return true;
+
+}
+
+
+// pop implementation
+template<typename T>
+bool RingBuffer<T>::pop(T &item) {
+    std::unique_lock<std::mutex> lock(mutex_);
+
+    cv_not_empty_.wait(lock, [this](){
+        return head_ != tail_ || shutdown_;
+    });
+
+    if(shutdown_ && head_ == tail_) {
+        return false;
+    }
+
+    item = buffer_[tail_];
+    tail_ = (tail_ + 1) % capacity_;
+
+    lock.unlock();
+    cv_not_full_.notify_one();
+
+    return true;
+}
+
+
+// shutdown implementation
+template<typename T>
+void RingBuffer<T>::shutdown() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    shutdown_ = true;
+    cv_not_full_.notify_all();
+    cv_not_empty_.notify_all();
+}
